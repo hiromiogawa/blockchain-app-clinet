4db7743c3c8e18e01412fe6cb83b899ae043dffd
hiromi <hiromi.ogawa.1223@gmail.com>
2023-05-14 19:50:13 +0900

    update: TransactionContext追加

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -1,1 +1,19 @@
 import { ethers } from "ethers";
+import { contractABI, contractAddrress } from "../utils/connect";
+
+// スマートコントラクトを取得
+const getSmartContract = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const signer = provider.getSigner();
+
+  const transactionContract = new ethers.Contract(
+    contractAddrress,
+    contractABI,
+    signer
+  );
+
+  console.log(provider, signer, transactionContract);
+
+  return transactionContract;
+};
