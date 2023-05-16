3c79a08f0f842957a3423cf95553f752add24fb3
hiromi <hiromi.ogawa.1223@gmail.com>
2023-05-16 20:15:43 +0900

    update: metamaskアカウント取得関数作成

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -1,12 +1,14 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { ethers } from "ethers";
 import { contractAbi, contractAddress } from "../utils/connect";
 
 export const TransactionContext = createContext();
 
+const { ethereum } = window;
+
 // スマートコントラクトを取得
 const getSmartContract = () => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
   const transactionContract = new ethers.Contract(
     contractAddress,
@@ -20,6 +22,18 @@ const getSmartContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
+  // metamaskウォレットと連携しているのか確認
+  const checkMetamaskWolletConnected = async () => {
+    if (!ethereum) return alert("メタマスクをインストールしてください");
+
+    // metamaskのアカウントIDを取得
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  };
+
+  useEffect(() => {
+    checkMetamaskWolletConnected();
+  }, []);
   return (
     <TransactionContext.Provider value={{ name: "shincode" }}>
       {children}
